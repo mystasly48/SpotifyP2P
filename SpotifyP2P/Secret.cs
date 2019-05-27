c683293d52a3baf39876566b5b42c7022dcf4a85
Maisu <mystasly48@gmail.com>
2019-05-28 00:00:37 +0900

    added Secret keys

---
+++ SpotifyP2P/Secret.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifyP2P {
+  class Secret {
+    // https://developer.spotify.com/my-applications/#!/applications
+    public static string ClientId = "";
+    public static string ClientSecret = "";
+  }
+}
